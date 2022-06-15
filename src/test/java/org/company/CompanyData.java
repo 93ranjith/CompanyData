daf139d6ce04706e61c230e8aae6062572d26e72
93ranjith <kumaresanravirani@gmail.com>
2022-06-15 12:44:31 +0530

    ranjit comp details

---
+++ src/test/java/org/company/CompanyData.java
@@ -0,0 +1,22 @@
+package org.company;
+
+public class CompanyData {
+	
+	private void emolyeedetails() {
+		System.out.println("total no of workers");
+
+	}
+	
+	private void empolyeeNO() {
+	
+		System.out.println("total no of workers is 200 ");
+
+	}
+	public static void main(String[] args) {
+		 
+		CompanyData sd =new CompanyData();
+		
+		sd.emolyeedetails();
+	}
+
+}
