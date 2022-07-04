22fc0718f22ee614d3216f91fc11bca5e1ac90a8
93ranjith <kumaresanravirani@gmail.com>
2022-07-04 09:59:16 +0530

    ram work done

--- src/test/java/org/company/CompanyData.java
+++ src/test/java/org/company/CompanyData.java
@@ -3,16 +3,14 @@ package org.company;
 public class CompanyData {
 	
 	private void employeeId() {
+		
 		System.out.println("employee ID :5088 ");
 
 	}
 	private void employeeName() {
 		System.out.println(" name of employee ");
 	}
-	private void emolyeedetails() {
-		System.out.println("total no of workers");
-
-	}
+	
 	
 	private void empolyeeNO() {
 	
@@ -23,7 +21,7 @@ public class CompanyData {
 		 
 		CompanyData sd =new CompanyData();
 		
-		sd.emolyeedetails();
+		sd.employeeName();
 	}
 
 }
